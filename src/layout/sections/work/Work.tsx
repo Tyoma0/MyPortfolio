6e33b61e1fa945730de56954b04d90e96308da36
Tyoma0 <artyom0070071@mail.ru>
2024-12-29 13:23:50 +0300

    h

--- src/layout/sections/work/Work.tsx
+++ src/layout/sections/work/Work.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import { SectionTitle } from '../../../components/SectionTitle';
+import { FlexWrapper } from '../../../components/FlexWrapper';
+import { Works } from './works/Works';
 
 export const Work = () => {
     return (
         <StyledWork>
-          <SectionTitle>Projects</SectionTitle>
+        <SectionTitle>Projects</SectionTitle>
+        <FlexWrapper>
+            <Works />
+            <Works />
+            <Works />
+            <Works />
+        </FlexWrapper>
         </StyledWork>
     );
 };
---
+++ src/layout/sections/work/works/Works.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Works = () => {
+    return (
+        <StyledWorks>
+            <Image src='' alt=''/>
+            <Title></Title>
+            <Text></Text>
+            <Link href={'a'}>Look It Up</Link>
+        </StyledWorks>
+    );
+};
+
+
+const StyledWorks = styled.div`
+    
+`
+const Image = styled.img`
+    
+`
+const Title = styled.h3`
+    
+`
+const Text = styled.p`
+    
+`
+const Link = styled.a`
+    
+`
