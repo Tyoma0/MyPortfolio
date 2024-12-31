3847ac61cee93178ecd9acbeedf130f6092d2439
Tyoma0 <artyom0070071@mail.ru>
2024-12-31 18:18:01 +0300

    g

---
+++ src/layout/sections/experience/Experience.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Experience = () => {
+    return (
+        <StyledExperience>
+            
+        </StyledExperience>
+    );
+};
+
+const StyledExperience = styled.section`
+    
+`
