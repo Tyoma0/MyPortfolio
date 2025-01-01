22dcb1bdfea4616ac9cf9ed733797b7ea67df793
Tyoma0 <artyom0070071@mail.ru>
2025-01-01 11:49:42 +0300

    h

---
+++ src/layout/sections/experience/experienceLine/ExperienceLine.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const ExperienceLine = () => {
+    return (
+        <Timeline>
+             <TimelinePoint>
+        <TimelineDate>01.01.2023</TimelineDate>
+        <TimelineText>Начало проекта</TimelineText>
+    </TimelinePoint>
+        </Timeline>
+    );
+};
+
+const Timeline = styled.div`
+    
+`
+const TimelinePoint = styled.div`
+    
+`
+const TimelineDate = styled.div`
+    
+`
+const TimelineText = styled.div`
+    
+`
