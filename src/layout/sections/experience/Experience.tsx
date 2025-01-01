06cb67a2f47f60019c26e094ecdad9609ddcd9be
Tyoma0 <artyom0070071@mail.ru>
2025-01-01 12:03:41 +0300

    g

--- src/layout/sections/experience/Experience.tsx
+++ src/layout/sections/experience/Experience.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import { SectionTitle } from '../../../components/SectionTitle';
+import { ExperienceLine } from './experienceLine/ExperienceLine';
 
 export const Experience = () => {
     return (
         <StyledExperience>
             <SectionTitle>Experience</SectionTitle>
+            <ExperienceLine/>
+            
         </StyledExperience>
     );
 };
 
 const StyledExperience = styled.section`
-    
+    background-color:#161D2A;
+    height:100vh
 `
--- src/layout/sections/experience/experienceLine/ExperienceLine.tsx
+++ src/layout/sections/experience/experienceLine/ExperienceLine.tsx
@@ -5,7 +5,19 @@ export const ExperienceLine = () => {
     return (
         <Timeline>
              <TimelinePoint>
-        <TimelineDate>01.01.2023</TimelineDate>
+        <TimelineDate>2017</TimelineDate>
+        <TimelineText>Начало проекта</TimelineText>
+    </TimelinePoint>
+    <TimelinePoint>
+        <TimelineDate>2019</TimelineDate>
+        <TimelineText>Начало проекта</TimelineText>
+    </TimelinePoint>
+    <TimelinePoint>
+        <TimelineDate>2021</TimelineDate>
+        <TimelineText>Начало проекта</TimelineText>
+    </TimelinePoint>
+    <TimelinePoint>
+        <TimelineDate>2023</TimelineDate>
         <TimelineText>Начало проекта</TimelineText>
     </TimelinePoint>
         </Timeline>
@@ -13,14 +25,40 @@ export const ExperienceLine = () => {
 };
 
 const Timeline = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 80%;
+    position: relative;
+   background-color:#ffff;
+   max-height:8px;
+   background: linear-gradient(to right, #945dd6, #6978d1, #13adc7);        
     
 `
 const TimelinePoint = styled.div`
-    
+     position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    z-index: 1;
+    background-color:#ffff;
+    max-width:25px;
+    max-height:25px;
+    border-radius:50%
 `
 const TimelineDate = styled.div`
+    font-weight: bold;
     
-`
+    text-align: center; 
+    color:#ffff;
+    position: absolute;
+    top:-90%
+    `
 const TimelineText = styled.div`
+    font-size: 14px;
+    color: #ffff;
+    margin-top: 30px; /* Отступ между точкой и текстом */
+    text-align: center;
     
+
 `
